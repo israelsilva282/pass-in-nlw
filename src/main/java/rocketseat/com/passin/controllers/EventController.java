25eae3e29726a8eff2cb91f625c6cfda0040421c
israelsilva282 <israelcostas@hotmail.com>
2024-04-05 13:40:36 -0300

    Adicionado funcionalidades de ver evento especifico e adicionar evento

---
+++ src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -0,0 +1,31 @@
+package rocketseat.com.passin.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import rocketseat.com.passin.dto.event.EventIdDTO;
+import rocketseat.com.passin.dto.event.EventRequestDTO;
+import rocketseat.com.passin.dto.event.EventResponseDTO;
+import rocketseat.com.passin.services.EventService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/events")
+public class EventController {
+    public final EventService service;
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id){
+        EventResponseDTO event = this.service.getEventDetail(id);
+        return ResponseEntity.ok(event);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+        EventIdDTO eventIdDTO = this.service.createEvent(body);
+
+        var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
+
+        return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+}
